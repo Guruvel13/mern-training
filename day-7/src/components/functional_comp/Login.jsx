18884d4b174fc609e0b7b4147b3e957d7099f01d
Guruvel13 <letters2guruvel@gmail.com>
2025-01-23 19:40:57 +0530

    devp

--- day-7/src/components/functional_comp/Login.jsx
+++ day-7/src/components/functional_comp/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../../CSS/Login.css';
+import './../../CSS/Login.css';
 
 const Login = () => {
     const handleSubmit = (event) => {
